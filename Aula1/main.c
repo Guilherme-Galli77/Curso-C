ccfef65b91064d64c33e996fec4814fb616be67b
Guilherme Cury Galli <guigalli2009@hotmail.com>
2021-08-25 15:30:59 -0300

    Update

---
+++ Aula1/main.c
@@ -0,0 +1,19 @@
+//Aula sobre variaveis
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Declarando variaveis
+    int idade;
+
+    //Função para escrever no console
+    printf("Qual eh a sua idade? \n");
+
+    //Receber dados
+    scanf("%d", &idade);
+
+    printf("A sua idade eh %d", idade);
+
+    return 0;
+}
